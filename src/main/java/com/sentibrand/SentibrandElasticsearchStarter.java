d4d42972878d56c8b2b7908c3f719bc318a86678
Fergus Strange <fergus.strange@infinityworks.com>
2015-12-30 23:05:44 +0000

    Add bootstrap class and method to start app. Change default port to 8888.

---
+++ src/main/java/com/sentibrand/SentibrandElasticsearchStarter.java
@@ -0,0 +1,11 @@
+package com.sentibrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentibrandElasticsearchStarter {
+    public static void main(String... args) {
+        SpringApplication.run(SentibrandElasticsearchStarter.class, args);
+    }
+}
---
+++ src/test/java/com/sentibrand/SentibrandElasticsearchStarterTest.java
@@ -0,0 +1,22 @@
+package com.sentibrand;
+
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SentibrandElasticsearchStarterTest {
+
+    @Test
+    public void shouldStartWebApplication() throws Exception {
+        SentibrandElasticsearchStarter.main();
+
+        HttpStatus statusCode = new RestTemplate()
+                .getForEntity("http://localhost:8888/health", String.class)
+                .getStatusCode();
+
+        assertThat(statusCode.value(), is(200));
+    }
+}
