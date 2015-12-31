0016182ee1ab34ed856fea8148413f9fbbcf980f
Fergus Strange <fergus.strange@infinityworks.com>
2015-12-31 11:51:31 +0000

    Add dynamic property to specify cluster name.

--- src/main/java/com/sentibrand/ElasticsearchConfiguration.java
+++ src/main/java/com/sentibrand/ElasticsearchConfiguration.java
@@ -2,22 +2,28 @@ package com.sentibrand;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Configuration
 @Profile("!embedded-es")
 public class ElasticsearchConfiguration {
 
+    @Value("${elastic.cluster.name:elasticsearch_ferguss}")
+    private String clusterName;
+
     @Bean
-    public Client client() throws UnknownHostException {
-        return TransportClient.builder()
+    public Client client() {
+        return TransportClient
+                .builder()
+                .settings(Settings.builder().put("cluster.name", clusterName).build())
                 .build()
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLocalHost(), 9200));
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLoopbackAddress(), 9300));
     }
 }
