a154972db22914451ff253414cf328adef66736d
Fergus Strange <fergus.strange@infinityworks.com>
2016-01-01 21:36:43 +0000

    Add custom java 8 date handling.

---
+++ src/main/java/com/sentibrand/JacksonConfiguration.java
@@ -0,0 +1,20 @@
+package com.sentibrand;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+}
